6f36285c1eb949f47e2dd3e83f4ca73e7d6daa1d
Garuna414 <anuragnalkar414@gmail.com>
2024-12-01 16:40:41 +0530

    Responsive Projects Section Added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,25 +4,25 @@ export default function About() {
   const headings = ["EDUCATION", "EXPERIENCE", "GOAL", "HOBBIES"];
   const content = [
     "I am a final year student, currently pursuing my Bachelors of Engineering in Computer Engineering from University of Mumbai with 8.7 CGPA.",
-    "I worked at Meta Craftlab Pvt. Ltd. as a Full Stack Developer Intern from June 2024 - July 2024. I was responsible for development of the organisations web app project modules along with other interns, remotely, using SvelteKit and Prisma.",
+    "I worked at Meta Craftlab Pvt. Ltd. as a Full Stack Developer Intern from June 2024 - July 2024. I was responsible for development of Craftlabs web app project modules along with other interns, remotely, using SvelteKit and Prisma.",
     "My goal is to become an experienced developer and assume a senior position at a respected organisation, contributing to cutting edge projects and advancing the organisations success.",
     "My hobbies includes photography of the skies. It can be in the form of photos, videos, timelapses and long exposures. I also like to listen to electronic music.",
   ];
   return (
     <div className="min-h-screen h-screen border-x-black flex flex-col p-5">
       <div>
-        <p className="md:text-5xl text-3xl text-red-600 font-mono">
+        <p className="lg:text-5xl md:text-4xl text-2xl text-red-600 font-mono">
           ABOUT ME
         </p>
       </div>
       <div className="md:w-2/3 flex md:flex-row flex-col h-full">
-        <div className="flex flex-col md:h-full h-auto md:justify-around md:gap-0 gap-3">
+        <div className="flex flex-col md:h-full h-auto md:justify-around sm:gap-0 gap-2">
           {headings.map((heading, key) => (
             <div key={key}>
-              <p className="md:text-3xl text-xl text-red-600 font-mono">
+              <p className="lg:text-3xl md:text-2xl text-xl text-red-600 font-mono text-center">
                 {heading}
               </p>
-              <p className="text-justify md:text-lg font-serif">{content[key]}</p>
+              <p className="text-justify lg:text-lg sm:text-base text-sm font-serif">{content[key]}</p>
             </div>
           ))}
         </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import React from "react";
+import blockchainWebsite from "../assets/blockchainWebsite.png";
 
 export default function Projects() {
+  const titles = ["BLOCKCHAIN BASED AUCTION WEBSITE"];
+  const images = [blockchainWebsite];
+  const content = [
+    "Developed an auction website to provide a secure platform for people to host and participate in auctions.",
+  ];
+  const stack = [["REACT", "EXPRESS", "SQL", "SOLIDITY"]];
   return (
-    <div>Projects</div>
-  )
+    <div className="p-5 flex flex-col gap-5 h-screen">
+      <div>
+        <p className="md:text-5xl text-3xl text-red-600 font-mono">PROJECTS</p>
+      </div>
+      <div className="flex flex-row h-full items-center">
+        {titles.map((title, index) => (
+          <div className="lg:w-1/3 md:w-1/2 h-fit border border-black rounded-3xl flex flex-col p-2">
+            <div className="h-auto w-full">
+              <img
+                src={images[index]}
+                alt="Blockchain_Website"
+                className="rounded-3xl lg:max-w-[30vw] md:w-auto m-auto"
+              />
+            </div>
+            <div className="flex flex-col gap-5 py-5 px-2">
+              <p className="font-mono text-red-600">{title}</p>
+              <p className="font-serif text-justify">{content[index]}</p>
+              <div>
+                <p className="font-mono text-red-600 mb-2">TECH STACK</p>
+                <ul className="flex flex-row flex-wrap gap-2">
+                  {stack[index].map((tech, key) => (
+                    <li
+                      key={key}
+                      className="rounded-3xl px-2 py-1 font-mono border border-black text-sm md:text-base"
+                    >
+                      {tech}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
