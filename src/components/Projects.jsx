b4550c4b6d5d00c25c4c4116e9393d8ed4f9b422
Garuna414 <anuragnalkar414@gmail.com>
2024-12-01 22:41:39 +0530

    Project section not displaying properly fixed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,18 +13,18 @@ export default function Projects() {
   ];
   const stack = [["REACT", "EXPRESS", "SQL", "SOLIDITY"],["REACT", "APIs"], ["C#", "UNITY 2D"]];
   return (
-    <div className="p-5 flex flex-col gap-5 h-screen w-full" id="Projects">
+    <div className="p-5 flex flex-col gap-5 h-auto w-full" id="Projects">
       <div>
         <p className="lg:text-5xl md:text-4xl text-2xl text-red-600 font-mono w-full">PROJECTS</p>
       </div>
       <div className="flex flex-row h-full gap-5 overflow-x-scroll flex-nowrap w-full">
         {titles.map((title, index) => (
-          <div className="lg:w-1/3 md:w-1/2 h-auto max-h-screen border border-black rounded-3xl flex flex-col p-2" key={index}>
+          <div className="lg:w-1/3 md:w-1/2 md:min-w-[30vw] min-w-[80vw] h-auto border border-black rounded-3xl flex flex-col p-2" key={index}>
             <div className="h-auto w-full">
               <img
                 src={images[index]}
                 alt="Blockchain_Website"
-                className="rounded-3xl lg:max-w-[30vw] md:w-auto m-auto max-w-30vw"
+                className="rounded-3xl w-auto m-auto"
               />
             </div>
             <div className="flex flex-col gap-5 py-5 px-2">
