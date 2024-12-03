48ad546c3e0c6a8c92be96ca9e3ffe7da0a4621d
Garuna414 <anuragnalkar414@gmail.com>
2024-12-04 00:29:39 +0530

    Fixing homepage URL

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import ProjectView from "./components/ProjectView";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Pfolio24">
       <div className="overflow-x-hidden">
         <div>
           <Navbar />
         </div>
         <div className="overflow-x-hidden mt-[68px] bg-gradient-to-br from-[#ffd4d4] to-[#f5f5f5]">
           <Routes>
+            <Route path="/" element={<Navigate to="/home" />} />
             <Route path="/home" element={<Home />} />
             <Route path="/projects/:name" element={<ProjectView />} />
           </Routes>
