ca00912368d55f907d7294c95389be71d19eef0e
Garuna414 <anuragnalkar414@gmail.com>
2024-12-10 13:08:01 +0530

    Updated favicon for the website, implemented routing using hashrouter, instead of browserRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Home from "./components/Home";
 import ProjectView from "./components/ProjectView";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <div className="overflow-x-hidden bg-gradient-to-br from-[#ffd4d4] to-[#f5f5f5]">
         <Routes>
           <Route path="/" element={<Home />} />
@@ -12,6 +17,6 @@ export default function App() {
           <Route path="/projects/:name" element={<ProjectView />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </Router>
   );
 }
